ca55b70fb575af9ab4776a5f045f8d7249f4449c
LeeJeongSeok <ljs0429777@gmail.com>
2021-02-26 14:17:24 +0900

    update : json 데이터를 백엔드로 넘겨서 객체 갯수 카운팅 코드 추가

--- src/main/java/com/lee/practice/steveleedemo/controller/Step4Controller.java
+++ src/main/java/com/lee/practice/steveleedemo/controller/Step4Controller.java
@@ -1,29 +1,30 @@
 package com.lee.practice.steveleedemo.controller;
 
 import com.lee.practice.steveleedemo.domain.User;
+import org.json.simple.JSONArray;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletResponse;
 import java.util.ArrayList;
+import java.util.List;
 
 @Controller
 public class Step4Controller {
 
-    @PostMapping("/step4")
-    public String step4(@RequestBody User user) {
-
-        System.out.println(user);
-
-        // 넘어온 데이터를 바로 객체에 집어넣는다.
-        ArrayList<User> list = new ArrayList<>();
-
-        System.out.println(list.size());
-
+    @GetMapping("/step4")
+    public String show() {
         return "step4";
+    }
+
+
+    @PostMapping("/step4")
+    public @ResponseBody String step4(@RequestBody List<User> user) {
+
+        System.out.println(user.size());
+
+
+        return "ok";
     }
 }
